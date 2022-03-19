e800e85e92a45a0d3217623d56248249f3512762
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2022-03-19 10:54:00 +0000

    set the "word" field from commandline args

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("abc");
+        final String word = args.length > 0 ? args[0] : "abc";
+        System.out.println(word);
     }
 }
