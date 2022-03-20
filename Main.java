00888faecfde79a1257df4866a882a4973d6beec
no one <detarame@example.com>
2022-03-20 13:10:22 +0900

    ファイル参照時に拡張子".txt"のもののみ抽出

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         final String word = args.length > 0 ? args[0] : "abc";
         System.out.println(word);
+
+        File[] files = new File("./").listFiles();
+        for (File file : files) {
+            String fileName = file.toString();
+            int fileNameNumber = fileName.length();
+            String extension = fileName.substring(fileNameNumber - 4, fileNameNumber);
+            if (extension.equals(".txt")) {
+                System.out.println(fileName);
+            }
+        }
     }
 }
